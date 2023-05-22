68e2878bc4785b12ae505c0a955c3a947554f20b
Amit Kumar <43449014+amitravikumar@users.noreply.github.com>
2023-05-22 21:02:38 +0530

    Update

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <>
+            <h1>Profile</h1>
+            <p>Name: </p>
+            <p>Age: </p>
+            <p>Email: </p>
+        </>
+    )
+}
+
+export default Profile;
